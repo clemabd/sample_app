e406b7559ffbeb12cfd5f7635782077feb0f58ac
Clémentine ABDOU <clementine.abdou@gmail.com>
2015-06-16 12:27:59 +0100

    Exercice page stat OK

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
+  before(:each) do
+    @base_title = "Simple App du Tutoriel Ruby on Rails | "
+
+  end
   describe "GET #home" do
     it "returns http success" do
       get :home
@@ -10,7 +14,7 @@ RSpec.describe PagesController, type: :controller do
     end
     it "doit avoir le bon titre" do
       get :home
-      expect(response.body).to have_selector("head title", text: "Simple App du Tutoriel Ruby on Rails | Accueil", visible: false)
+      expect(response.body).to have_selector("head title", text: @base_title + "Accueil", visible: false)
     end
   end
 
@@ -21,7 +25,7 @@ RSpec.describe PagesController, type: :controller do
     end
     it "doit avoir le bon titre" do
       get :contact
-      expect(response.body).to have_selector("head title", text:"Simple App du Tutoriel Ruby on Rails | Contact" , visible: false)
+      expect(response.body).to have_selector("head title", text:@base_title + "Contact" , visible: false)
     end
   end
 
@@ -32,10 +36,19 @@ RSpec.describe PagesController, type: :controller do
       end
     it "doit avoir le bon titre" do
       get :about
-      expect(response.body).to have_selector("head title", text: "Simple App du Tutoriel Ruby on Rails | A propos", visible: false)
+      expect(response.body).to have_selector("head title", text: @base_title + "A propos", visible: false)
     end
   end
 
-
+  describe "GET #help" do
+    it "returns http success" do
+      get :help
+      expect(response).to have_http_status(:success)
+    end
+    it "doit avoir le bon titre" do
+      get :help
+      expect(response.body).to have_selector("head title", text: @base_title + "Aide", visible: false)
+    end
+  end
 
 end
